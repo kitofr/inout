d01ffc50ac90f64db41f10c3cf68a5654836d1b3
Kristoffer Roupé <kitofr@gmail.com>
2016-09-13 21:08:07 +0200

    Foo

--- web/elm/InOut.elm
+++ web/elm/InOut.elm
@@ -1,6 +1,32 @@
 module InOut exposing (main)
 
+import Platform.Cmd as Cmd exposing (Cmd)
 import Html exposing (..)
+import Html.App as App exposing (..)
+import Html.Attributes as A
+import List exposing (..)
 
+main : Program Never
 main =
-  Html.text "Hello from ELM"
+  App.program { init = init
+               , view = view
+               , update = update
+               , subscriptions = \_ -> Sub.none
+               }
+
+type alias Model = String
+type Msg =
+  Foo
+
+init : (Model, Cmd Msg)
+init = 
+    ("Foo", Cmd.none)
+
+
+view : Model -> Html Msg
+view model =
+  div [] [text model] 
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  (model, Cmd.none)
