99a7324c667807a7463ff8d544009d047143bcf3
Kristoffer Roupé <kitofr@gmail.com>
2016-09-21 22:22:20 +0200

    HTTP.post/get and view items

--- web/elm/InOut.elm
+++ web/elm/InOut.elm
@@ -7,10 +7,14 @@ import Html.Events exposing (onClick)
 import Html.Attributes exposing (class)
 import List exposing (..)
 import Http
-import Json.Encode
+import Json.Encode as Encode
 import Json.Decode exposing (Decoder, decodeValue, succeed, string, list, (:=))
 import Json.Decode.Extra exposing ((|:))
-import Task
+import Task exposing (Task)
+
+getUrl : String
+--getUrl = "http://localhost:4000/events"
+getUrl = "https://inout-backend.herokuapp.com/events"
 
 main : Program Never
 main =
@@ -24,6 +28,7 @@ type alias Event =
     { updated_at : String
     , status : String
     , location : String
+    , device : String
     , inserted_at : String
     }
 
@@ -36,29 +41,40 @@ type Msg =
   | Load 
   | FetchSucceed Model
   | FetchFail Http.Error
+  | HttpSuccess String
+  | HttpFail Http.Error
+    
 
 init : (Model, Cmd Msg)
 init = 
     ({ events = [] },  Cmd.none)
 
 eventItem event =
-  li [ class "event" ] [ text event.status]
+  let color = if event.status == "check-in" then "success" else "info"
+  in
+    li [ class ("list-group-item list-group-item-" ++ color) ] 
+        [h5 [class "list-group-item-heading"] [text event.status]
+        ,p [class "list-group-item-text"] [text event.inserted_at]
+        ,p [class "list-group-item-text"] [text event.location]
+        ]
 
 eventsComponent events =
-  [div []
-    [text "Events: "]
-    , ul [ class "events" ]
-      (List.map eventItem events)
-  ]
+  div []
+    [h3 [] [text "Events: "]
+     , ul [ class "list-group" ]
+      (List.map eventItem (List.reverse events))
+    ]
+  
 
 view : Model -> Html Msg
 view model =
   div [] 
     [div []
-    (eventsComponent model.events) 
-    , button [class ("button"), onClick Load ] [text "load"]
-    , button [class ("button is-primary")] [text "check in"]
-    , button [class ("button is-primary")] [text "check out"]
+      [ button [class ("btn"), onClick Load ] [text "load"]
+      , button [class ("btn btn-success"), onClick CheckIn] [text "check in"]
+      , button [class ("btn btn-primary"), onClick CheckOut] [text "check out"]
+      , (eventsComponent model.events)
+      ]
     ]
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -70,18 +86,43 @@ update msg model =
       (newModel, Cmd.none)
     FetchFail error -> 
       (model, Cmd.none)
-    _ -> 
+    CheckIn ->
+      (model, (check "in"))
+    CheckOut ->
+      (model, (check "out"))
+    HttpFail error ->
+      let _ = Debug.log "error" error
+      in
       (model, Cmd.none)
-
+    HttpSuccess things ->
+      let _ = Debug.log "success" things
+      in
+      (model, getEvents)
 
 -- HTTP
+post : Decoder value -> String -> Http.Body -> Task Http.Error value
+post decoder url body =
+  let request =
+        { verb = "POST"
+        , headers = [("Content-Type", "application/json")]
+        , url = url
+        , body = body
+        }
+  in
+      Http.fromJson decoder (Http.send Http.defaultSettings request)
+
+
+check : String -> Cmd Msg
+check inOrOut=
+  let rec = Debug.log "encode" encodeEvent { status = "check-" ++ inOrOut, location = "work" }
+  in
+      Task.perform HttpFail HttpSuccess 
+        (post (succeed "") getUrl (Debug.log "payload" (Http.string rec)))
 
 getEvents : Cmd Msg
 getEvents =
-  let url = "http://localhost:4000/events"
-      get = (Http.get decodeEvents url)
-  in
-      Task.perform FetchFail FetchSucceed get 
+  Task.perform FetchFail FetchSucceed 
+    (Http.get decodeEvents getUrl)
 
 decodeEvents : Json.Decode.Decoder Model
 decodeEvents =
@@ -93,14 +134,16 @@ decodeEvent =
     Json.Decode.succeed Event
         |: ("updated_at" := Json.Decode.string)
         |: ("status" := Json.Decode.string)
+        |: ("device" := Json.Decode.string)
         |: ("location" := Json.Decode.string)
         |: ("inserted_at" := Json.Decode.string)
 
-encodeEvent : Event -> Json.Encode.Value
+encodeEvent : { status: String, location: String } -> String
 encodeEvent record =
-    Json.Encode.object
-        [ ("updated_at",  Json.Encode.string <| record.updated_at)
-        , ("status",  Json.Encode.string <| record.status)
-        , ("location",  Json.Encode.string <| record.location)
-        , ("inserted_at",  Json.Encode.string <| record.inserted_at)
-        ]
+    Encode.encode 0 (Encode.object
+        [("event", Encode.object [
+          ("status",  Encode.string <| record.status)
+          , ("location",  Encode.string <| record.location)
+          , ("device",  Encode.string <| "internetz")
+          ])
+        ])
