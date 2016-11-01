40ed3be6764b86316484ad0b8265ad3160d1d722
Kristoffer Roupé <kitofr@gmail.com>
2016-11-01 22:46:26 +0100

    Elm format

--- web/elm/Api.elm
+++ web/elm/Api.elm
@@ -7,33 +7,45 @@ import Http
 import Task exposing (Task)
 import Types exposing (..)
 
+
 post : Decoder value -> String -> Http.Body -> Task Http.Error value
 post decoder url body =
-  let request =
-        { verb = "POST"
-        , headers = [("Content-Type", "application/json")]
-        , url = url
-        , body = body
-        }
-  in
-      Http.fromJson decoder (Http.send Http.defaultSettings request)
+    let
+        request =
+            { verb = "POST"
+            , headers = [ ( "Content-Type", "application/json" ) ]
+            , url = url
+            , body = body
+            }
+    in
+        Http.fromJson decoder (Http.send Http.defaultSettings request)
+
 
 check : String -> String -> Cmd Msg
-check inOrOut hostUrl=
-  let rec = Debug.log "encode" encodeEvent { status = "check-" ++ inOrOut, location = "tv4play" }
-  in
-      Task.perform HttpFail HttpSuccess
-        (post (succeed "") (hostUrl ++ "/events") (Debug.log "payload" (Http.string rec)))
+check inOrOut hostUrl =
+    let
+        rec =
+            Debug.log "encode" encodeEvent { status = "check-" ++ inOrOut, location = "tv4play" }
+    in
+        Task.perform
+            HttpFail
+            HttpSuccess
+            (post (succeed "") (hostUrl ++ "/events") (Debug.log "payload" (Http.string rec)))
+
 
 getEvents : String -> Cmd Msg
-getEvents hostUrl=
-  Task.perform FetchFail FetchSucceed
-    (Http.get decodeEvents (hostUrl ++ "/events.json"))
+getEvents hostUrl =
+    Task.perform
+        FetchFail
+        FetchSucceed
+        (Http.get decodeEvents (hostUrl ++ "/events.json"))
+
 
 decodeEvents : JD.Decoder (List Event)
 decodeEvents =
-  JD.succeed identity
-    |: ("events" := JD.list decodeEvent)
+    JD.succeed identity
+        |: ("events" := JD.list decodeEvent)
+
 
 decodeEvent : JD.Decoder Event
 decodeEvent =
@@ -43,12 +55,17 @@ decodeEvent =
         |: ("inserted_at" := Extra.date)
         |: ("updated_at" := Extra.date)
 
-encodeEvent : { status: String, location: String } -> String
+
+encodeEvent : { status : String, location : String } -> String
 encodeEvent record =
-    Encode.encode 0 (Encode.object
-        [("event", Encode.object [
-          ("status",  Encode.string <| record.status)
-          , ("location",  Encode.string <| record.location)
-          , ("device",  Encode.string <| "internetz")
-          ])
-        ])
+    Encode.encode 0
+        (Encode.object
+            [ ( "event"
+              , Encode.object
+                    [ ( "status", Encode.string <| record.status )
+                    , ( "location", Encode.string <| record.location )
+                    , ( "device", Encode.string <| "internetz" )
+                    ]
+              )
+            ]
+        )
