0f458cae706fce951eb318a36ada59b2e0b26bb4
Kristoffer Roupé <kitofr@gmail.com>
2016-11-01 21:28:33 +0100

    Rainbow!

--- web/elm/Charts.elm
+++ web/elm/Charts.elm
@@ -4,6 +4,30 @@ import List
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Seq
+
+
+rainbowColors =
+    [ "#f80c12"
+    , "#ee1100"
+    , "#ff3311"
+    , "#ff4422"
+    , "#ff6644"
+    , "#ff9933"
+    , "#feae2d"
+    , "#ccbb33"
+    , "#d0c310"
+    , "#aacc22"
+    , "#69d025"
+    , "#22ccaa"
+    , "#12bdb9"
+    , "#11aabb"
+    , "#4444dd"
+    , "#3311bb"
+    , "#3b0cbd"
+    , "#442299"
+    ]
+
 
 barChart : List { a | hour : number } -> Html msg
 barChart dayCount =
@@ -11,9 +35,9 @@ barChart dayCount =
         (List.indexedMap
             (\i day ->
                 rect
-                    [ stroke "#777"
-                    , fill ("#7a" ++ (toString (i % 10))) 
+                    [ stroke "#333" 
                     , strokeWidth "1"
+                    , fill (Seq.nth (i % (List.length rainbowColors)) rainbowColors "#442299")
                     , x ((toString (i + 1)) ++ "0")
                     , y "5"
                     , width "9"
