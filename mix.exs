061c5429a9e6e56761908d12b9d1d8dc1e1d2d57
Kristoffer Roupé <kitofr@gmail.com>
2017-06-12 23:01:18 +0200

    Update phoenix

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Inout.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Inout, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
                     :phoenix_ecto, :postgrex, :httpotion]]
   end
 
@@ -30,9 +30,10 @@ defmodule Inout.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
+    [{:phoenix, "~> 1.2.0"},
+     {:phoenix_pubsub, "~> 1.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
+     {:phoenix_ecto, "~> 3.0-rc"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:comeonin, "~> 1.0"},
