4c4dc1610514c5bc562c1816caa8f6f8a000fc9e
Kristoffer Roupé <kitofr@gmail.com>
2017-06-21 08:48:27 +0200

    inout-backend -> inochut

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :inout, Inout.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "inout-backend.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "inochut.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
