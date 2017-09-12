89f03a746fb1131dd15a5c1fff8050cac86718b9
Kristoffer Roupé <kitofr@gmail.com>
2017-09-12 17:27:58 +0200

    WIP dateinput

---
+++ web/elm/InputExtra.elm
@@ -0,0 +1,14 @@
+module InputExtra exposing (dateInput, timeInput)
+
+import Html exposing (..)
+import Html.Attributes as Attr exposing (..)
+
+
+dateInput : List (Html.Attribute msg) -> List (Html msg) -> Html msg
+dateInput attr children =
+    input (attr ++ [ type_ "date", step "1", Attr.min "2017-01-01" ]) children
+
+
+timeInput : List (Html.Attribute msg) -> List (Html msg) -> Html msg
+timeInput attr children =
+    input (attr ++ [ type_ "time", step "5" ]) children
