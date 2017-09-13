8e0f6527b342ca2d8ee379b944fca62e48406d0e
Kristoffer Roupé <kitofr@gmail.com>
2017-09-13 21:29:02 +0200

    Some styling

--- web/elm/EditEvent.elm
+++ web/elm/EditEvent.elm
@@ -16,16 +16,19 @@ editEvent event =
         _ =
             Debug.log "edit event" event
     in
-        li []
-            [ span [] [ text ((toString event.id) ++ ". " ++ event.status ++ " ") ]
-            , dateInput [ onInput (DateUpdated event), value (dateStr event.inserted_at) ] []
-            , timeInput [ onInput (TimeUpdated event), value (timeStr event.inserted_at) ] []
-            , button [ onClick (Update event) ] [ text "Update" ]
-            , button [ onClick (Delete event) ] [ text "Delete" ]
+        li [ class "list-group-item" ]
+            [ 
+              div [ ] [
+                span [] [ text ((toString event.id) ++ ". " ++ event.status ++ " ") ]
+              , dateInput [ onInput (DateUpdated event), value (dateStr event.inserted_at) ] []
+              , timeInput [ onInput (TimeUpdated event), value (timeStr event.inserted_at) ] []
+              , button [ onClick (Update event) ] [ text "Update" ]
+              , button [ onClick (Delete event) ] [ text "Delete" ]
+              ]
             ]
 
 
 edit : DayItem -> Html Msg
 edit dayItem =
-    ul [ class "row" ]
+    ul [ class "list-group" ]
         (List.map editEvent dayItem.events)
