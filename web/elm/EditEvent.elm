2456d7322d2fd1cbb0dadd31926b499f80c0999e
Kristoffer Roupé <kitofr@gmail.com>
2017-09-13 21:42:11 +0200

    More styling

--- web/elm/EditEvent.elm
+++ web/elm/EditEvent.elm
@@ -19,11 +19,11 @@ editEvent event =
         li [ class "list-group-item" ]
             [ 
               div [ ] [
-                span [] [ text ((toString event.id) ++ ". " ++ event.status ++ " ") ]
-              , dateInput [ onInput (DateUpdated event), value (dateStr event.inserted_at) ] []
-              , timeInput [ onInput (TimeUpdated event), value (timeStr event.inserted_at) ] []
-              , button [ onClick (Update event) ] [ text "Update" ]
-              , button [ onClick (Delete event) ] [ text "Delete" ]
+                span [style [("width", "100px"), ("display","inline-block")]] [ text ((toString event.id) ++ ". " ++ event.status ++ " ") ]
+              , dateInput [ style [("margin-left", "20px")], onInput (DateUpdated event), value (dateStr event.inserted_at) ] []
+              , timeInput [ style [("margin-left", "20px")], onInput (TimeUpdated event), value (timeStr event.inserted_at) ] []
+              , button [ style [("margin-left", "20px")], class "btn btn-success", onClick (Update event) ] [ text "Update" ]
+              , button [ class "btn btn-danger", onClick (Delete event) ] [ text "Delete" ]
               ]
             ]
 
