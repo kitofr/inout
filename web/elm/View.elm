99b3df9d46ad032f9a7a1ec84370f8a93239f646
Kristoffer Roupé <kitofr@gmail.com>
2017-10-05 15:09:27 +0200

    Elm analyze

--- web/elm/View.elm
+++ web/elm/View.elm
@@ -1,44 +1,44 @@
-module View exposing (..)
+module View exposing (view)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (text, p, h5, li, Html, div, ul, span, h3, button)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
-import Date exposing (..)
-import Date.Extra.Compare as Compare exposing (is, Compare2(..))
-import Dict exposing (get, empty)
-import Date.Extra.Duration as Duration exposing (..)
+import Date
+import Date.Extra.Compare exposing (Compare2(SameOrBefore))
+import Dict
+import Date.Extra.Duration exposing (DeltaRecord)
 import Charts exposing (barChart)
-import DateUtil exposing (..)
-import Types exposing (..)
-import Msgs exposing (..)
-import Seq exposing (..)
+import DateUtil exposing (dateToMonthStr, periodToStr, TimeDuration, addTimeDurations, emptyTimeDuration, toTimeDuration, toMonthStr, monthOrder, sortDates)
+import Types exposing (Event, emptyEvent, timeDifference, DayItem, Model)
+import Msgs exposing (Msg(ViewEvent))
+import Seq exposing (groupBy, desc)
 import Last5 exposing (last5)
 import EditEvent exposing (edit)
 import TimeSinceLastCheckIn exposing (viewTimeSinceLastCheckIn)
-import ViewMsgs exposing (ViewMsg(..))
+import ViewMsgs exposing (ViewMsg(Load, CheckIn, CheckOut))
 
 
-eventItem : Event -> Html Msg
-eventItem event =
-    let
-        color =
-            if event.status == "check-in" then
-                "success"
-            else
-                "info"
-    in
-        li [ class ("list-group-item list-group-item-" ++ color) ]
-            [ h5 [ class "list-group-item-heading" ] [ text event.status ]
-            , p [ class "list-group-item-text" ] [ text <| dateToMonthStr event.inserted_at ]
-            , p [ class "list-group-item-text" ] [ text event.device ]
-            , p [ class "list-group-item-text" ] [ text event.location ]
-            ]
+-- eventItem : Event -> Html Msg
+-- eventItem event =
+--     let
+--         color =
+--             if event.status == "check-in" then
+--                 "success"
+--             else
+--                 "info"
+--     in
+--         li [ class ("list-group-item list-group-item-" ++ color) ]
+--             [ h5 [ class "list-group-item-heading" ] [ text event.status ]
+--             , p [ class "list-group-item-text" ] [ text <| dateToMonthStr event.inserted_at ]
+--             , p [ class "list-group-item-text" ] [ text event.device ]
+--             , p [ class "list-group-item-text" ] [ text event.location ]
+--             ]
 
 
 monthItem : { count : Int, year : Int, month : String, total : TimeDuration, monthlyDayCount : List { hour : Int, minute : Int } } -> Html Msg
 monthItem month =
-    li [ class ("list-group-item list-group-item-success row") ]
-        [ h5 [ class "list-group-item-heading" ] [ text (month.month ++ " " ++ (toString month.year)) ]
+    li [ class "list-group-item list-group-item-success row" ]
+        [ h5 [ class "list-group-item-heading" ] [ text (month.month ++ " " ++ toString month.year) ]
         , p [ class "list-group-item-text monthly-hours col-md-6" ] [ text (periodToStr month.total) ]
         , p [ class "list-group-item-text monthly-count col-md-2" ] [ text (toString month.count) ]
         , p [ class "list-group-item-text monthly-chart col-md-6" ] [ barChart month.monthlyDayCount ]
@@ -50,13 +50,14 @@ monthlySum month =
     List.foldl addTimeDurations emptyTimeDuration (List.map (\y -> toTimeDuration y.diff) month)
 
 
-totalsRect x =
+totalsRect : ( Int, List { a | date : Date.Date, diff : { day : Int, millisecond : Int, month : Int, second : Int, year : Int, hour : Int, minute : Int } } ) -> { count : Int, month : String, monthlyDayCount : List { hour : Int, minute : Int }, total : TimeDuration, year : Int }
+totalsRect rec =
     let
         data =
-            (Tuple.second x)
+            Tuple.second rec
 
         year =
-            case (List.head data) of
+            case List.head data of
                 Just d ->
                     Date.year d.date
 
@@ -64,7 +65,7 @@ totalsRect x =
                     0
     in
         { year = year
-        , month = toMonthStr (Tuple.first x)
+        , month = toMonthStr (Tuple.first rec)
         , total = monthlySum data
         , count =
             List.length data
@@ -73,6 +74,21 @@ totalsRect x =
         }
 
 
+monthlyTotals :
+    List
+        { a
+            | diff :
+                { day : Int
+                , hour : Int
+                , millisecond : Int
+                , minute : Int
+                , month : Int
+                , second : Int
+                , year : Int
+                }
+            , date : Date.Date
+        }
+    -> Html Msg
 monthlyTotals sorted =
     let
         perMonth =
@@ -108,21 +124,20 @@ sortedDayItems events =
             groupBy (\x -> dateToMonthStr x.inserted_at) events
 
         dayItems =
-            (List.map
+            List.map
                 (\x ->
                     let
                         date =
                             (List.head (Tuple.second x) |> Maybe.withDefault emptyEvent).inserted_at
                     in
-                        { dateStr = (Tuple.first x)
-                        , diff = (timeDifference (Tuple.second x))
+                        { dateStr = Tuple.first x
+                        , diff = timeDifference (Tuple.second x)
                         , date = date
                         , dayNumber = Date.day date
                         , events = Tuple.second x
                         }
                 )
                 (Dict.toList grouped)
-            )
     in
         dayItems |> List.sortWith (\a b -> sortDates SameOrBefore a.date b.date)
 
@@ -130,7 +145,7 @@ sortedDayItems events =
 yearTab : ( Int, List Event ) -> Html Msg
 yearTab ( year, list ) =
     span [ class "tab" ]
-        [ h3 [] [ text ("Montly totals for " ++ (toString year)) ]
+        [ h3 [] [ text ("Montly totals for " ++ toString year) ]
         , monthlyTotals (sortedDayItems list)
         ]
 
@@ -139,8 +154,7 @@ eventsComponent : List Event -> Html Msg
 eventsComponent events =
     let
         groupedByYear =
-            -- Debug.log "grouped by year"
-            (groupBy (\x -> Date.year x.inserted_at) events)
+            groupBy (\x -> Date.year x.inserted_at) events
                 |> Dict.toList
                 |> List.sortWith (\( a, _ ) ( b, _ ) -> desc a b)
 
@@ -166,14 +180,14 @@ view model =
                     div [] []
     in
         div []
-            [ div [ class ("container") ]
-                [ div [ class ("row") ]
-                    [ button [ class ("btn"), onClick (ViewEvent Load) ] [ text "refresh" ]
-                    , button [ class ("btn btn-success"), onClick (ViewEvent CheckIn) ] [ text "check in" ]
-                    , button [ class ("btn btn-primary"), onClick (ViewEvent CheckOut) ] [ text "check out" ]
+            [ div [ class "container" ]
+                [ div [ class "row" ]
+                    [ button [ class "btn", onClick (ViewEvent Load) ] [ text "refresh" ]
+                    , button [ class "btn btn-success", onClick (ViewEvent CheckIn) ] [ text "check in" ]
+                    , button [ class "btn btn-primary", onClick (ViewEvent CheckOut) ] [ text "check out" ]
                     ]
-                , div [ class ("row check-timer") ] (viewTimeSinceLastCheckIn model.timeSinceLastCheckIn)
+                , div [ class "row check-timer" ] (viewTimeSinceLastCheckIn model.timeSinceLastCheckIn)
                 , shouldEdit
-                , (eventsComponent model.events)
+                , eventsComponent model.events
                 ]
             ]
