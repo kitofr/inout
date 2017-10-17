917f263b1a35b66a086fcbe93ec8a3d46aed011f
Kristoffer Roupé <kitofr@gmail.com>
2017-10-17 13:24:17 +0200

    Format

--- web/elm/Charts.elm
+++ web/elm/Charts.elm
@@ -3,7 +3,7 @@ module Charts exposing (..)
 import List
 import Html exposing (Html)
 import Svg exposing (svg, rect)
-import Svg.Attributes exposing (viewBox, width, stroke, strokeWidth, fill, x, y, width, height, transform)
+import Svg.Attributes exposing (viewBox, width, stroke, strokeWidth, fill, x, y, height, transform)
 import Seq
 
 
@@ -47,17 +47,21 @@ barChart dayCount =
         svg [ viewBox ("0 0 " ++ viewSizeStr ++ " 110"), width (viewSizeStr ++ "px") ]
             (List.indexedMap
                 (\i day ->
-                    rect
-                        [ stroke "#333"
-                        , strokeWidth "1"
-                        , fill (Seq.nth (day.hour % (List.length rainbowColors)) rainbowColors "#442299")
-                        , x (toString ((i * barSize) + offSet))
-                        , y "5"
-                        , width ((barSize - 2) |> toString)
-                        , height ((toString (1 + day.hour)) ++ "0")
-                        , transform "rotate(180) translate(-300 -100)"
-                        ]
-                        []
+                    let
+                        color =
+                            Seq.nth (day.hour % (List.length rainbowColors)) rainbowColors "#442299"
+                    in
+                        rect
+                            [ stroke "#333"
+                            , strokeWidth "1"
+                            , fill color
+                            , x (toString ((i * barSize) + offSet))
+                            , y "5"
+                            , width ((barSize - 2) |> toString)
+                            , height ((toString (1 + day.hour)) ++ "0")
+                            , transform "rotate(180) translate(-300 -100)"
+                            ]
+                            []
                 )
                 (List.reverse dayCount)
             )
