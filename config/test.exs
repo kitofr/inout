991f7378ea98c2e2a18888a0f738da44eb2763e3
Kristoffer Roupé <kitofr@gmail.com>
2018-01-16 17:18:18 +0100

    Use db

--- config/test.exs
+++ config/test.exs
@@ -15,5 +15,5 @@ config :inout, Inout.Repo,
   username: "postgres",
   password: "postgres",
   database: "inout_test",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
