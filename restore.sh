61ce83a6adcb0d6a3ce80b24b41f6e0702a2b484
Kristoffer Roupé <kitofr@gmail.com>
2018-10-26 11:19:52 +0200

    Update with comment...

--- restore.sh
+++ restore.sh
@@ -6,3 +6,6 @@ LOCAL_DUMP_PATH="latest.dump"
 docker-compose up -d db
 docker exec -i "${DOCKER_DB_NAME}" pg_restore -C --verbose --clean --no-acl --no-owner -U "${DB_USER}" -d "${DB_HOSTNAME}" < "${LOCAL_DUMP_PATH}"
 docker-compose stop db
+
+echo "Remember to rename the created database to inout_dev!"
+echo "ALTER DATABASE db RENAME TO newdb"
