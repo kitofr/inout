c8d396b4455c51eed55ae8cd25361f6866cf81ff
Kristoffer Roupé <kitofr@gmail.com>
2019-01-21 18:37:19 +0100

    Upgrade elixir and erlang

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Inout.Mixfile do
   def project do
     [app: :inout,
      version: "0.0.2",
-     elixir: "~> 1.4.5",
+     elixir: "~> 1.7.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
