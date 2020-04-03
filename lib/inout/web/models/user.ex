47cd7f3587c80316d1c943863b6ba92fc104ff53
Kristoffer Roupé <kitofr@gmail.com>
2020-04-03 14:50:11 +0200

    :empty -> %{}

--- lib/inout/web/models/user.ex
+++ lib/inout/web/models/user.ex
@@ -4,6 +4,10 @@ defmodule Inout.Web.User do
   alias Inout.Web.Event
   alias Inout.Web.Contract
 
+  @derive {
+    Jason.Encoder,
+    only: [:email, :password]
+  }
   schema "users" do
     field :email, :string
     field :crypted_password, :string
@@ -16,13 +20,7 @@ defmodule Inout.Web.User do
   @required_fields ~w(email password)
   @optional_fields ~w()
 
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> unique_constraint(:email)
